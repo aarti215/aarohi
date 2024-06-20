be79b3b17145a7ea315a45327e3b8457aa42dc37
Aarti Lonare <aarulonare2002@gmail.com>
2024-06-20 17:26:20 +0530

    Delete module directory

--- module/EC2/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "aarti" {
-  ami = var.ami_id
-  instance_type = var.instance_type
-  key_name = "terraform-key"
-  availability_zone = "us-east-1a"
-  
-}
--- module/s3/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucket-1" {
-  bucket = "var973849"
-   
-}
